256a8f76a55d74cb763dc779fbcdc68eb17ec46b
Lammy23 <lammyaluko@gmail.com>
2023-11-25 12:42:24 -0700

    Created a general header file for all four classes to use. Created a flight.cpp module for the implementation of the flight class.

---
+++ general.h
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Airline{};
+
+class Flight {
+
+    private:
+    string flight_id;
+    int rows;
+    int columns;
+    vector<Passenger> passenger_list;
+    vector<vector<string>> seat_map;
+    
+
+    public:
+    Flight(string flight_id, int rows, int columns);
+    ~Flight(){};
+    Flight(const Flight &flight);
+    Flight& operator=(const Flight &flight);
+
+    const string get_flight_id() const{ return flight_id;};
+    const int get_rows() const{ return rows;};
+    const int get_columns() const{ return columns;};
+
+    void set_flight_id(string flight_id){ this->flight_id = flight_id;};
+    void set_rows(int rows){ this->rows = rows;};
+    void set_columns(int columns){ this->columns = columns;};
+
+    void add_passenger(Passenger passenger);
+    void remove_passenger(Passenger passenger);
+
+    void showFlightSeatMap();
+};
+
+class Passenger {
+    private:
+    string id;
+
+    public:
+    string get_id() const{ return id;};
+};
+
+class Seat {};
