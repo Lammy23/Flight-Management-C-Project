6820cdef3985ffc6c6207be777cd5ec0096db898
Lammy23 <lammyaluko@gmail.com>
2023-11-29 20:06:59 -0700

    Modularized code and added airline The pull request contains the following changes:
    - Created folders contained the now split up code.
    
    - Created a main.cpp to how global function defintions and main code interface.
    
    - Created the airline class and defined some functions

---
+++ passenger/passenger.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "flight/flight.h"
+
+using namespace std;
+
+class Passenger
+{
+private:
+    int passengerID;
+    string firstName;
+    string lastName;
+    int phoneNumber;
+    Flight *passengerSeat; // FLIGHT CLASS
+
+public:
+    Passenger(int id, string fName, string lName, int phone, Seat *seat);
+    Passenger(const Passenger &other);
+    ~Passenger() {}
+
+    int getPassengerID() const { return passengerID; }
+    string getFirstName() const { return firstName; }
+    string getLastName() const { return lastName; }
+    int getPhoneNumber() const { return phoneNumber; }
+    Flight *getPassengerSeat() const { return passengerSeat; }
+
+    void setPassengerID(int id) { passengerID = id; }
+    void setFirstName(const string &fName) { firstName = fName; }
+    void setLastName(const string &lName) { lastName = lName; }
+    void setPhoneNumber(int phone) { phoneNumber = phone; }
+    // void setPassengerSeat(Seat* seat) { passengerSeat = seat; } // Lami commented this line of code
+    void showPassengerInfo(); // Lami added this line of code
+};
---
+++ seat/seat.h
@@ -0,0 +1,19 @@
+
+class Seat
+{
+private:
+    int rowNumber;
+    char columnLetter;
+    bool status;
+
+public:
+    Seat(int rowNum, char colLetter, bool stat);
+    Seat(const Seat &other);
+    ~Seat() {}
+    int getRowNumber() const { return rowNumber; }
+    char getColumnLetter() const { return columnLetter; }
+    bool getStatus() const { return status; }
+    void setRowNumber(int rowNum) { rowNumber = rowNum; }
+    void setColumnLetter(char colLetter) { columnLetter = colLetter; }
+    void setStatus(bool stat) { status = stat; }
+};
